9a9777eefbd1887f8196524121d744a5fad7d8d2
xuyifei <xuyifei@youdi.net>
2020-09-12 14:21:37 +0800

    添加服务器上传代码

---  README.md
+++  README.md
@@ -1,10 +1,15 @@
 #`用到的工具包`
-   1、commander   其作用是让node命令行程序的制作更加简单，封装后的node命令行接口
-#`配置`
-   1、添加全局命令的时候需要在`cli`的头部标明当前的执行环境
+1、commander 其作用是让 node 命令行程序的制作更加简单，封装后的 node 命令行接口 #`配置`
+1、添加全局命令的时候需要在`cli`的头部标明当前的执行环境
 
+# npm 常用命令
 
-# npm常用命令
-   npm login
-   npm publish
-   npm unpublish --force  删除刚刚发布的包
+npm login
+npm publish
+npm unpublish --force 删除刚刚发布的包
+
+# npm 建立本地包的链接
+
+在当前包的下面执行 `sudo npm link`
+
+### 注意：在入口文件需要添加 #!/usr/bin/env node
--- demo/deploy_config/deploy.config.js
+++ demo/deploy_config/deploy.config.js
@@ -1,32 +1,34 @@
 module.exports = {
-    dev: {
-        localFilePath: '/Users/piguanzheng/Documents/youdi_platform/buyer_app/publish/dev',
-        webDir: "/opt/www/youdi-platform/app",  //上传到服务器的位置
-        // commond: './run.sh',
-        projectName: '',
-        prefentSh: async (exec, dir, ora) => {
-            try {
-                console.log('开始打包')
-                exec('npm run build', { cwd: dir });
-                console.log('打包成功,开始压缩上传');
-            } catch (err) {
-                process.exit(1);
-            }
-        },
-        afterSh: 'rm -rf pad && mv dist pad',
-        //服务器相关配置
-        password: '',
-        username: "",
-        host: '',
-        port: "22",
+  dev: {
+    localFilePath:
+      "/Users/piguanzheng/Documents/youdi_platform/buyer_app/publish/dev",
+    webDir: "/opt/www/youdi-platform/app", //上传到服务器的位置
+    // commond: './run.sh',
+    projectName: "",
+    prefentSh: async (exec, dir, ora) => {
+      try {
+        console.log("开始打包");
+        exec("npm run build", { cwd: dir });
+        console.log("打包成功,开始压缩上传");
+      } catch (err) {
+        process.exit(1);
+      }
     },
-    pro: {
-        localFilePath: '/Users/piguanzheng/Documents/study/github_study/auto_upload/deploy_x/dist/index.html',
-        webDir: "/opt/www/youdi-platform/ceshi",  //上传到服务器的位置
-        //服务器相关配置
-        password: '',
-        username: "web",
-        host: '',
-        port: ""
-    }
-}
+    afterSh: "rm -rf pad && mv dist pad",
+    //服务器相关配置
+    password: "",
+    username: "",
+    host: "",
+    port: "22",
+  },
+  pro: {
+    localFilePath:
+      "/Users/piguanzheng/Documents/study/github_study/auto_upload/deploy_x/dist/index.html",
+    webDir: "/opt/www/youdi-platform/ceshi", //上传到服务器的位置
+    //服务器相关配置
+    password: "",
+    username: "web",
+    host: "",
+    port: "",
+  },
+};
